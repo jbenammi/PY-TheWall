c5df26f3436b30a52d0be91da388e2a44950302c
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-05-08 20:21:36 -0700

    updated error handling on the login page

--- server.py
+++ server.py
@@ -13,34 +13,26 @@ def main():
 
 @app.route('/login', methods = ['POST'])
 def validate_login():
-	if 'registered' in session:
-		del session['registered']
-	if 'err_email2' in session:
-		del session['err_email2']
-	if 'err_password2' in session:
-		del session['err_password2']		
-	sess_count = 0
+	session['errors'] = {}
 	if not EMailRegex.match(request.form['email']):
-		session['err_email2'] = "The E-Mail must be a valid e-mail address"
-		sess_count += 1
+		session['errors'].update({'email2': 'The E-Mail must be a valid e-mail address'})
 	if len(request.form['password']) < 8:
-		session['err_password2'] = "Password must be at least 8 characters"
-		sess_count += 1
-	if sess_count > 0:
+		session['errors'].update({'password2': 'Password must be at least 8 characters'})
+	if len(session['errors']) > 0:
 		return redirect('/')
 	else:
 		query = "SELECT * FROM users WHERE email = :email LIMIT 1"
 		data = {'email': request.form['email']}
 		user = mysql.query_db(query, data)
 		if user == []:
-			session['notreg'] = "E-Mail is not registered."	
+			session['errors'].update({'notreg': 'E-Mail is not registered.'})	
 			return redirect('/')
 		else:
 			if bcrypt.check_password_hash(user[0]['password'], request.form['password']):
 				session['logged_info'] = {'id': user[0]['id'], 'first_name': user[0]['first_name'], 'last_name': user[0]['last_name']}
 				return redirect('/wall')
 			else:
-				session['passmatch'] = "E-Mail or Password does not match registered information"
+				session['errors'].update({'passmatch': 'Incorrect password entered for registered E-Mail.'})
 				return redirect('/')
 
 @app.route('/wall')
@@ -103,74 +95,32 @@ def remove_warning():
 
 @app.route('/process', methods = ['POST'])
 def validate_():
-
-	errors = []
-	if 'notreg' in session:
-		del session['notreg']
-	if 'err_fname' in session:
-		del session['err_fname']
-	if 'err_lname' in session:
-		del session['err_lname']
-	if 'err_email' in session:
-		del session['err_email']
-	if 'err_password' in session:
-		del session['err_password']
-	if 'err_confirm' in session:
-		del session['err_confirm']
-	sess_count = 0
+	session['errors'] = {}
 	if len(request.form['fname']) < 2:
-		errors += {'fname': 'The first name field must be at least two characters'}
-		session['err_fname'] = "The first name field must be at least two characters"
-		sess_count += 1
+		session['errors'].update({'fname': 'The first name field must be at least two characters'})
 	elif not str.isalpha(str(request.form['fname'])):
-		errors += {'fname': 'First name cannot have number or symbols'}
-		session['err_fname'] = "First name cannot have number or symbols"
-		sess_count += 1
+		session['errors'].update({'fname': 'First name cannot have number or symbols'})
 	if len(request.form['lname']) < 2:
-		errors += {'lname': 'The last name field must be at least two characters'}
-		session['err_lname'] = "The last name field must be at least two characters"
-		sess_count += 1
+		session['errors'].update({'lname': 'The last name field must be at least two characters'})
 	elif not str.isalpha(str(request.form['lname'])):
-		errors += {'lname': 'Last name cannot have numbers or symbols'}
-		session['err_lname'] = "Last name cannot have numbers or symbols"
-		sess_count += 1
+		session['errors'].update({'lname': 'Last name cannot have numbers or symbols'})
 	if not EMailRegex.match(request.form['email']):
-		errors += {'email': 'The E-Mail must be a valid e-mail address'}
-		session['err_email'] = "The E-Mail must be a valid e-mail address"
-		sess_count += 1
+		session['errors'].update({'email': 'The E-Mail must be a valid e-mail address'})
 	if len(request.form['password']) < 8:
-		errors += {'password': 'Password must be at least 8 characters'}
-		session['err_password'] = "Password must be at least 8 characters"
-		sess_count += 1
+		session['errors'].update({'password': 'Password must be at least 8 characters'})
 	elif not any(char.isdigit() for char in str(request.form['password'])):
-		errors += {'password': 'Password must contain at least one number'}
-		session['err_password'] = "Password must contain at least one number"
-		sess_count += 1
+		session['errors'].update({'password': 'Password must contain at least one number'})
 	elif not any(char.isupper() for char in str(request.form['password'])):
-		errors += {'password': 'Password must contain at least one uppercase letter'}
-		session['err_password'] = "Password must contain at least one uppercase letter"
-		sess_count += 1
+		session['errors'].update({'password': 'Password must contain at least one uppercase letter'})
 	if request.form['confirmpass'] != request.form['password']:
-		errors += {'confirmpass': 'The password confirmation does not match the password'}
-		session['err_confirm'] = "The confirmation does not match the password"
-		sess_count += 1
-	if len(errors) > 0:
-		return render_template('main.html', errors = errors)
+		session['errors'].update({'confirmpass': 'The password confirmation does not match the password'})
+	if len(session['errors']) > 0:
+		return render_template('main.html')
 	else:
-		if 'err_fname' in session:
-			del session['err_fname']
-		if 'err_lname' in session:
-			del session['err_lname']
-		if 'err_email' in session:
-			del session['err_email']
-		if 'err_password' in session:
-			del session['err_password']
-		if 'err_confirm' in session:
-			del session['err_confirm']
-		session['registered'] = "True"
 		query1 = "SELECT email FROM users WHERE email = :email"
 		data1 = {"email": request.form['email']}
 		if not mysql.query_db(query1, data1):
+			session['registered'] = "registered"
 			pw_hash = bcrypt.generate_password_hash(request.form['password'])
 			query = "INSERT INTO users(first_name, last_name, email, password, created_on, updated_on) VALUES(:first_name, :last_name, :email, :password, now(), now())"
 			info = {
@@ -181,7 +131,7 @@ def validate_():
 			}
 			mysql.query_db(query, info)
 		else:
-			session['user_registered'] = "This E-Mail is already registered"
+			session['errors'].update({'user_registered': 'This E-Mail is already registered'})
 		return redirect('/')
 
 @app.route('/logout')
